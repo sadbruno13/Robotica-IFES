c780b3dc21d2d29781b77a14adcda1e873712c88
sadbruno13 <bruno.pratti2@gmail.com>
2023-05-09 21:23:24 -0300

    New Code
    Adicionando controle via SERIAL, além de adicionar calculo para os angulos

--- moveRobot/moveRobot.ino
+++ moveRobot/moveRobot.ino
@@ -1,49 +1,91 @@
 #include <AccelStepper.h>
 
-// define as portas do driver A4988 conectadas ao CNC Shield
-#define dirPin1 2
-#define stepPin1 3
-#define dirPin2 5
-#define stepPin2 6
+// Define os pinos para o motor 1
+#define motor1_passo 2
+#define motor1_direcao 5
 
-// define a velocidade e o número de passos por revolução do motor
-#define velocidade 1000
-#define passos_por_revolucao 200
+// Define os pinos para o motor 2
+#define motor2_passo 3
+#define motor2_direcao 6
 
-AccelStepper stepper1(AccelStepper::DRIVER, stepPin1, dirPin1);
-AccelStepper stepper2(AccelStepper::DRIVER, stepPin2, dirPin2);
+// Define a relação entre os graus e os passos para o motor 1
+#define motor1_graus_para_passos 2.222222
+
+// Define a relação entre os graus e os passos para o motor 2
+#define motor2_graus_para_passos 2.222222
+
+// Inicializa os objetos AccelStepper para cada motor
+AccelStepper stepper1(AccelStepper::DRIVER, motor1_passo, motor1_direcao);
+AccelStepper stepper2(AccelStepper::DRIVER, motor2_passo, motor2_direcao);
 
 void setup() {
-  // inicia a comunicação serial com o PC
+  // Define as configurações iniciais dos motores
+  stepper1.setMaxSpeed(10000);
+  stepper1.setAcceleration(500);
+
+  stepper2.setMaxSpeed(10000);
+  stepper2.setAcceleration(500);
+
   Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    // lê os ângulos enviados pelo Python
-    int angulo1 = Serial.parseInt();
-    int angulo2 = Serial.parseInt();
+    // recebe os ângulos desejados para cada motor
+  int angulo1 = 0;
+  int angulo2 = 0;
+  Serial.println("Digite o angulo para o motor 1:");
+  while (angulo1 == 0) {
+    if (Serial.available() > 0) {
+     angulo1 = Serial.parseInt();
+    }
+  }
+  Serial.println("Digite o número de passos para o motor 2:");
+  while (angulo2 == 0) {
+    if (Serial.available() > 0) {
+      angulo2 = Serial.parseInt();
+    }
+  }
+    Serial.read(); // limpa o buffer do serial
 
-    // calcula o número de passos para cada motor
-    int passos1 = (angulo1 / 360.0) * passos_por_revolucao;
-    int passos2 = (angulo2 / 360.0) * passos_por_revolucao;
+    // converte os ângulos em passos
+    int passos1 = angulo1 * motor1_graus_para_passos;
+    Serial.println(passos1);
+    int passos2 = angulo2 * motor2_graus_para_passos;
 
-    // configura os motores de passo
-    stepper1.setMaxSpeed(velocidade);
-    stepper1.setAcceleration(velocidade);
-    stepper1.moveTo(passos1);
+    // calcula as velocidades proporcionais
+    float velocidade_proporcional1 = 1000.0;
+    float velocidade_proporcional2 = 1000.0;
+    if (passos1 >= passos2) {
+      velocidade_proporcional1 = velocidade_proporcional2*float((passos2/passos1));
+    } else {
+      velocidade_proporcional2 = velocidade_proporcional1*float((passos1/passos2));
+    }
 
-    stepper2.setMaxSpeed(velocidade);
-    stepper2.setAcceleration(velocidade);
-    stepper2.moveTo(passos2);
+    // ajusta as velocidades para que os motores terminem o movimento juntos
+    if (passos1 >= passos2) {
+      stepper1.setSpeed(velocidade_proporcional1);
+      stepper2.setSpeed(velocidade_proporcional2);
+    } else {
+      stepper1.setSpeed(velocidade_proporcional1);
+      stepper2.setSpeed(velocidade_proporcional2);
+    }
 
     // move os motores de passo
+    stepper1.moveTo(passos1);
+    stepper2.moveTo(passos2);
+
+    // aguarda o término do movimento dos motores
     while (stepper1.distanceToGo() != 0 || stepper2.distanceToGo() != 0) {
       stepper1.run();
       stepper2.run();
     }
-
-    // envia a resposta para o Python
-    Serial.println("Tarefa finalizada");
+    delay(3000);
+    
+    // move os motores de passo para origem (0,0)
+    stepper1.moveTo(0);
+    stepper2.moveTo(0);
+     while (stepper1.distanceToGo() != 0 || stepper2.distanceToGo() != 0) {
+      stepper1.run();
+      stepper2.run();
+     }
   }
-}
